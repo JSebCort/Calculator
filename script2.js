cfa0219bc4c414454a345c7274a66715261042a9
JSebCort <44817309+JSebCort@users.noreply.github.com>
2022-08-31 11:37:10 -0400

    Added comments to script2.js

--- script2.js
+++ script2.js
@@ -1,3 +1,4 @@
+// Variables that will hold the content of the displays and the operands/operators utilized.
 let mainDisplay = "";
 let subDisplay = "";
 let numOne = "";
@@ -5,6 +6,7 @@ let numTwo = "";
 let operator1 = "";
 let operator2 = "";
   
+// Simple functions to handle operations, given two operands.
 function add(x, y) {
     return (x)+(y);
 }
@@ -21,6 +23,7 @@ function divide(x, y) {
     return ((x)/(y)).toFixed(2);
 }
 
+// Given two operands and an operator, the corresponding function is called and the result is returned.
 function operate(operator, x, y) {
     x = parseInt(x);
     y = parseInt(y);
@@ -41,16 +44,24 @@ function operate(operator, x, y) {
     }
 }
 
+// Function to help understand the logic's use of these four variables.
 function check() {
     console.log("n1:",numOne,"n2:",numTwo,"op1:",operator1,"op2:",operator2);
 }
 
+/**
+ * When a number button is clicked, it appends the number to the main display.
+ * @param {*} x ; The number value corresponding to the button clicked.
+ */
 function onRun(x) {
     mainDisplay = mainDisplay + String(x);
     document.getElementById("valueDisplay").value = mainDisplay;
-
 }
 
+/**
+ * Disables all functionality of the buttons, except 'Clear'.
+ * This allows the 'Clear' button to re-enable the buttons.
+ */
 function disableButtons() {
     const numButtons = document.getElementsByClassName("numButtons");
     for (const input of numButtons){
@@ -58,6 +69,9 @@ function disableButtons() {
     }
 }
 
+/**
+ * Enables all functionality of the buttons.
+*/
 function enableButtons() {
     const numButtons = document.getElementsByClassName("numButtons");
     for (const input of numButtons){
@@ -65,12 +79,17 @@ function enableButtons() {
     }
 }
 
+/**
+ * The two displays are updated to show the current operation being handled.
+ */
 function updateDisplays() {
     document.getElementById('valueDisplay').value = mainDisplay;
     document.getElementById('functionDisplay').value = subDisplay;
-    
 }
 
+/**
+ * Resets all variables, displays and enables all buttons if the 'Clear' button is clicked.
+ */
 function clear() {
     document.getElementById('valueDisplay').value = "";
     document.getElementById('functionDisplay').value = "";
@@ -82,27 +101,41 @@ function clear() {
     operator2 = "";
     enableButtons();
 }
-
+/**
+ * Handles functionality of the calculator when operator buttons are clicked.
+ * @param {*} x ; The operator being passed if it has been clicked.
+ * @returns Updates the displays.
+ */
 function operator(x) {
     // If operator2 is the same as operator1, don't evaluate
     if (operator2 == x) { 
         return;
-    } else if (operator1 == "") {
+    }
+
+    // If there isn't already an operator, store the operator as operator1.
+    // Then it updates the displays and prepares for the next number.
+    else if (operator1 == "") {
         numOne = mainDisplay;
         operator1 = x;
         subDisplay = numOne+operator1;
         mainDisplay = "";
         updateDisplays();
-    } else if (operator1 != "") {
+    } 
+
+    // If a second operator is clicked, without having clicked the equals button, it calculates
+    // the operation using num1, num2 and operator1. Then the 2nd operator now becomes operator1.
+    else if (operator1 != "") {
         operator2 = x;
         numTwo = document.getElementById("valueDisplay").value = mainDisplay;
-
         let answer = String(operate(operator1,parseInt(numOne),parseInt(numTwo)));
+
+        // If the user tries to divide by 0, the buttons are disabled and the calculator must be cleared.
         if (answer == "Divided by 0") {
             document.getElementById("valueDisplay").value = answer;
             disableButtons();
             return;
         }
+
         subDisplay = numOne+operator1+numTwo+"="+answer;
         operator1 = operator2;
         numOne = answer;
@@ -114,21 +147,30 @@ function operator(x) {
     }
 }
 
-
+/**
+ * 
+ * @returns The result of the operation handled.
+ */
 function equals() {
+    // If an operator wasn't input before clicking equals, do nothing.
     if (operator1 == "" || document.getElementById('valueDisplay').value == "") {
         return;
     }
 
+    // Stores numTwo and handles the operation.
     numTwo = document.getElementById('valueDisplay').value;
     let answer = operate(operator1,parseInt(numOne),parseInt(numTwo));
 
+    // If the user tries to divide by 0, the buttons are disabled and the calculator must be cleared.
     if (answer == "Divided by 0") {
         document.getElementById("valueDisplay").value = answer;
         disableButtons();
         return;
     }
     
+    // If the previous calculations were done through multiple operators (rather than equals),
+    // this checks the subDisplay for an operator so it can correctly display the result.
+    // Else it simply displays the answer given the variables already stored.
     if (['*', '/', '-', '+'].includes(subDisplay.slice(-1))) {
         console.log('here')
         numOne = answer;
@@ -148,6 +190,6 @@ function equals() {
     }
 }
 
+// Calls the functions if the corresponding buttons are clicked.
 document.getElementById("clear").onclick = function(){clear()};
-
 document.getElementById("equals").onclick = function(){equals()};
